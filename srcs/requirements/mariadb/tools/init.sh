2be377bf79b981bab25cd52a723e0fef7b8df14b
thaisnishimoto <thaismnishimoto@gmail.com>
2025-04-17 16:42:27 -0300

    Fix mariadb init setup

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -1,22 +1,15 @@
 #!/bin/sh
 
 service mariadb start
-until mariadb -e "SELECT 1" 2>/dev/null
-do
-	sleep 1
+
+# Wait until the server is ready
+until mariadb -u root -e "SELECT 1;" &> /dev/null; do
+    echo "Waiting for MariaDB to be ready..."
+    sleep 1
 done
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Initializing database..."
 mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${WP_DB_NAME}; \
 CREATE USER IF NOT EXISTS '${WP_DB_USER}'@'%' IDENTIFIED BY '${WP_DB_PASSWORD}'; \
 GRANT ALL PRIVILEGES ON ${WP_DB_NAME}.* TO '${WP_DB_USER}'@'%'; \
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}'; \
 FLUSH PRIVILEGES;"
-
-else
-    echo "Database already initialized, skipping setup."
-
-fi
-
-service mariadb stop
