6f4fce5397516dec820d5a9f61fbe2e216908fac
thaisnishimoto <thaismnishimoto@gmail.com>
2025-04-18 23:33:17 -0300

    Added initial wordpress setup

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -4,31 +4,25 @@
 if [ ! -f /var/www/html/wp-config.php ]; then
   wp core download --allow-root --path=/var/www/html
 
-  wp config create \
-    --allow-root \
-    --path=/var/www/html \
+  wp config create --allow-root --path=/var/www/html \
     --dbname="$WP_DB_NAME" \
     --dbuser="$WP_DB_USER" \
     --dbpass="$WP_DB_PASSWORD" \
-    --dbhost=mariadb
+    --dbhost=mariadb:3306
 fi
 
 # Wait for MariaDB to be ready
 until wp db check --allow-root --path=/var/www/html; do
   echo "Waiting for MariaDB to be ready..."
-  sleep 2
+  sleep 1
 done
 
 # WordPress initial setup
-if [ ! -f /var/www/html/wp-config.php ]; then
-  wp core install \
-    --allow-root \
-    --path=/var/www/html \
-    --url="$DOMAIN_NAME" \
-    --title="Inception" \
-    --admin_user="$WP_ADMIN_USER" \
-    --admin_password="$WP_ADMIN_PASSWORD" \
-    --admin_email="$WP_ADMIN_EMAIL"
-fi
+wp core install --allow-root --path=/var/www/html \
+  --url="$DOMAIN_NAME" \
+  --title="Inception" \
+  --admin_user="$WP_ADMIN_USER" \
+  --admin_password="$WP_ADMIN_PASSWORD" \
+  --admin_email="$WP_ADMIN_EMAIL"
 
 exec php-fpm7.4 -F
